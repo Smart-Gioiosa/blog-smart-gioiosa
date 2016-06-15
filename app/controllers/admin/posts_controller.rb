e3f17552021d3908b732575000536f0e3f9f3bcb
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2016-06-15 15:23:02 +0200

    Added admin layout

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,9 +1,54 @@
-class Admin::PostsController < ApplicationController
+class Admin::PostsController < Admin::BaseController
+  before_action :set_post, only:[:show, :edit, :destroy, :update]
+
   def index
-    @posts = Post.all
+    @posts= Post.all
   end
- 
+
   def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Posts.new(post_params)
+    if @post.save
+      redirect_to admin_posts_path(@post)
+      flash[:success] = "Post Create"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      flash[:success] = "Post Aggiornato"
+      redirect_to admin_post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @post.destroy
+    flash[:success] = "Post Eliminato"
+    redirect_to profile_user_beauty_center_posts_path(@user, @beauty_center)
+  end
+
+
+  private
+  def set_post
+    @post =  Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  root to: "home#index"
   resources :posts, only:[:index, :show] 
 
 
@@ -9,6 +8,7 @@ Rails.application.routes.draw do
     resources :posts
   end
 
+  
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
