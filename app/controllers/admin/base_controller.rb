ea5ca10d474abd0b6343bd85d29a2d1212f37b2c
ALESSANDRO <alexscaffidi@hotmail.it>
2016-06-16 17:24:46 +0200

    added admin boolean to users

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,13 @@
 class Admin::BaseController < ActionController::Base
- before_action :authenticate_user! 
+ before_action :authenticate_user!
+ before_action :is_admin?
+
+def is_admin?
+  if current_user.admin?
+    true
+  else
+    redirect_back(fallback_location: root_path)
+  end
+end
  layout 'admin'
 end
