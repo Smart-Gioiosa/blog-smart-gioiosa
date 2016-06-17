5091e778fb1e7dba6494a6d2f07aef045ec4c20f
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2016-06-17 22:35:44 +0200

    Update disqus script

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIVLTM4CSMAZD5D7Q',
-    :aws_secret_access_key  => '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',
-    :region                 => 'eu-west-1'
-  }
-  config.fog_directory  = "smartgioiosa"
-end
