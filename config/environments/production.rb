3cfdca33fbe4ed3133b32239b39c2cdefcf26f2e
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2016-06-20 17:18:12 +0200

    Update disqus variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  #Rails.application.routes.default_url_options[:host] = 'smartgioiosa.it'
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
