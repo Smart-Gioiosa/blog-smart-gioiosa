f6b96c0581e10507f907bd18c339296c2407cc78
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2016-11-05 12:12:35 +0100

    Added SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Smart Gioiosa" 
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}" 
+    end
+  end 
+ 
+  def meta_descriptions(page_description = ' ')
+    base_description = "Blog Smart Gioiosa"
+    if page_description.empty?
+      tag('meta', name: 'description', content: "#{base_description}")
+    else
+      tag('meta', name: 'description', content: "#{base_description} - #{page_description}")
+
+    end
+  end
+
 end
