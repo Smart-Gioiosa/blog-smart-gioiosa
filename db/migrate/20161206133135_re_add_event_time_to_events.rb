b1d1c29486708dc1f7f97a4657b7e18a438a0fd7
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2016-12-06 14:56:50 +0100

    Updated field type

--- db/migrate/20161206133135_re_add_event_time_to_events.rb
+++ db/migrate/20161206133135_re_add_event_time_to_events.rb
@@ -1,5 +1,5 @@
 class ReAddEventTimeToEvents < ActiveRecord::Migration[5.0]
   def change
-     add_column :events, :event_time, :timestampz
+     add_column :events, :event_time, :datetime
   end
 end
