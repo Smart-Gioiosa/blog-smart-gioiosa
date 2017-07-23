0eb45d9987e09c113b0dd3b23a87280572d011b8
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2017-07-23 14:52:45 +0200

    Enable forgot password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.action_mailer.default_url_options = { host: 'www.smartgioiosa.it'}
   config.action_mailer.perform_deliveries = true
   config.active_support.deprecation =  :notify
   config.action_mailer.delivery_method = :smtp
